6a2c808dbddc0e8a1ba150bd551e05bfe2f73072
Evgeny Kabantsev <toorosan@yandex.ru>
2020-07-25 18:16:35 +0300

    Added mocked backend-part to enable UI development #6

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,5 +1,54 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"grpc-csv-viewer/internal/app/roles/client"
+)
+
+func uiHandler(w http.ResponseWriter, r *http.Request) {
+	_, err := fmt.Fprintf(w, "Hi there, I am future ui handler!")
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func timeSeriesHandler(w http.ResponseWriter, r *http.Request) {
+	mockTimeSeries := client.TimeSeries{
+		FileName:  "mocked-values.csv",
+		StartDate: time.Now().Add(-time.Hour * 3),
+		StopDate:  time.Now(),
+		Values: []client.SeriesItem{
+			{
+				Date:  time.Now().Add(-time.Hour * 3),
+				Value: 1,
+			},
+			{
+				Date:  time.Now().Add(-time.Hour * 2),
+				Value: 2,
+			},
+			{
+				Date:  time.Now().Add(-time.Hour * 1),
+				Value: 4,
+			},
+			{
+				Date:  time.Now(),
+				Value: 1,
+			},
+		},
+	}
+	err := json.NewEncoder(w).Encode(mockTimeSeries)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
 func main() {
-	print("gRPC client initiated, more content coming soon")
+	http.HandleFunc("/api/timeseries", timeSeriesHandler)
+	http.HandleFunc("/", uiHandler)
+	log.Fatal(http.ListenAndServe(":8081", nil))
 }
