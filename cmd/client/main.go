35db0d70b6bc77f5a23ba33836775ca4b8aaa230
Evgeny Kabantsev <toorosan@yandex.ru>
2020-07-26 18:26:30 +0300

    Fixed gRPC Client API endpoint according to #6

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -10,14 +10,14 @@ import (
 	"grpc-csv-viewer/internal/app/roles/client"
 )
 
-func uiHandler(w http.ResponseWriter, r *http.Request) {
+func uiHandler(w http.ResponseWriter, _ *http.Request) {
 	_, err := fmt.Fprintf(w, "Hi there, I am future ui handler!")
 	if err != nil {
 		log.Fatal(err)
 	}
 }
 
-func timeSeriesHandler(w http.ResponseWriter, r *http.Request) {
+func timeSeriesHandler(w http.ResponseWriter, _ *http.Request) {
 	mockTimeSeries := client.TimeSeries{
 		FileName:  "mocked-values.csv",
 		StartDate: time.Now().Add(-time.Hour * 3),
@@ -48,7 +48,7 @@ func timeSeriesHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	http.HandleFunc("/api/timeseries", timeSeriesHandler)
+	http.HandleFunc("/api/v1/timeseries", timeSeriesHandler)
 	http.HandleFunc("/", uiHandler)
 	log.Fatal(http.ListenAndServe(":8081", nil))
 }
